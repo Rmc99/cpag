fe43b84e0c91b411800f9c1c68bc710705ec3ba2
ctic <ronaldson@ifma.edu.br>
2019-02-02 15:06:59 -0300

    cpag

---
+++ pagamento/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+class Categoria(models.Models):
+    categoria = models.CharField(max_length=50, null=False, verbose_name="Categoria", blank=True)
+
+class Pagamento(models.Models):
+    MES_CHOICES = (
+        (1, "Janeiro"),
+        (2, "Fevereiro"),
+        (3, "Março"),
+        (4, "Abril"),
+        (5, "Maio"),
+        (6, "Junho"),
+        (7, "Julho"),
+        (8, "Agosto"),
+        (9, "Setembro"),
+        (10, "Outubro"),
+        (11, "Novembro"),
+        (12, "Dezembro"),
+    )
+    funcao = models.CharField(max_length=100, null=False, verbose_name="Função", blank=True)
+    qtd_horas = models.IntegerField(null=False, verbose_name="Quantidade de Horas no Mês", blank=True)
+    valor_hora = models.DecimalField(max_digits=5, decimal_places=2, null=False, verbose_name="Valor da Hora")
+    valor_bruto = models.DecimalField(max_digits=5, decimal_places=2, null=False, verbose_name="Valor Bruto")
+    valor_base_desc_inss = models.DecimalField(max_digits=5, decimal_places=2, null=False, verbose_name="Valor Base Desconto INSS")
+    valor_base_desc_iss = models.DecimalField(max_digits=5, decimal_places=2, null=False,
+                                              verbose_name="Valor Base Desconto ISS")
+    qtd_dependente_irpf = models.IntegerField(null=False, verbose_name="Quantidade de Dependentes de IRPF", blank=True)
+    valor_deducao_irpf = models.DecimalField(max_digits=5, decimal_places=2, null=False,
+                                              verbose_name="Valor Dedução IRPF")
+    valor_pos_deducao_irpf = models.DecimalField(max_digits=5, decimal_places=2, null=False,
+                                              verbose_name="Valor Pós Dedução IRPF")
+    valor_irpf = models.DecimalField(max_digits=5, decimal_places=2, null=False,
+                                              verbose_name="IRPF")
+    valor_liquido = models.DecimalField(max_digits=5, decimal_places=2, null=False,
+                                              verbose_name="Valor Líquido")
+    valor_patronal = models.DecimalField(max_digits=5, decimal_places=2, null=False,
+                                              verbose_name="Patronal")
+    ano = models.IntegerField(null=False, verbose_name="Ano de Pagamento", blank=True, help_text="Ex: 2019")
+    mes = models.IntegerField(null=False, verbose_name="Mês de Pagamento", blank=True)
---
+++ pessoa/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Pessoa(models.Models):
+    nome = models.CharField(max_length=150, null=False, verbose_name="Nome Completo", blank=True)
+    cpf = models.CharField(max_length=14, null=False, verbose_name="CPF", blank=True)
+    pis = models.CharField(max_length=25, null=False, verbose_name="PIS", blank=True)
+    num_conta = models.IntegerField(null=False, verbose_name="Conta", blank=True)
+    num_agencia = models.IntegerField(null=False, verbose_name="Agência", blank=True)
+    num_operacao = models.IntegerField(null=False, verbose_name="Operação", blank=True)
+    num_banco = models.IntegerField(null=False, verbose_name="Número do Banco", blank=True)
