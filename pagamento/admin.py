e6998418868abd4fa41df9fe054013bdc0b3fa4e
Rmc99 <ronaldson_castro@hotmail.com>
2019-02-08 14:41:38 -0300

    Calculos para servidor e colaborador interno

--- pagamento/admin.py
+++ pagamento/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 from .models import Pagamento
 from django.forms import TextInput
 from django.db import models
@@ -7,11 +8,6 @@ class PagamentoAdmin(admin.ModelAdmin):
     autocomplete_fields = ['pessoa']
     list_display = ('ano', 'mes', 'pessoa', 'categoria', 'funcao', 'valor_bruto', 'valor_irpf', 'valor_liquido')
     search_fields = ['ano', 'mes', 'categoria', 'funcao', 'pessoa__nome']
-    #fields = ['ano', 'mes', 'pessoa', 'categoria', 'funcao', 'qtd_horas',
-    #          'valor_hora', 'valor_pensao', 'qtd_dependente_irpf', 'valor_bruto', 'valor_base_desc_inss',
-    #          'valor_base_desc_iss', 'valor_deducao_irpf',
-    #          'valor_pos_deducao_irpf', 'valor_irpf', 'valor_liquido', 'valor_patronal']
-
     fieldsets = (
         ('Dados do Pagamento:', {'fields': (('ano', 'mes', 'pessoa'), ('categoria', 'funcao', 'qtd_horas'),
          ('valor_hora', 'valor_pensao', 'qtd_dependente_irpf'))}),
@@ -19,11 +15,18 @@ class PagamentoAdmin(admin.ModelAdmin):
              'valor_iss'), ('deducao_irpf', 'valor_pos_deducao_irpf', 'valor_irpf'), ('valor_liquido', 'valor_patronal'))}),
     )
 
+    def response_change(self, request, obj):
+        obj.ano = 4877
+        obj.save()
+        redirect_url = request.path
+        return HttpResponseRedirect(redirect_url)
+
+
     class Media:
         js = ('js/pagamento/jquery-3.3.1.min.js',)
         js = ('js/pagamento/jquery.mask.min.js',)
 #        js = ('js/pagamento/jquery-calculo.js',)
-        js = ('js/pagamento/jquery-mymask-pagamento.js',)
+#        js = ('js/pagamento/jquery-mymask-pagamento.js',)
 
 
     formfield_overrides = {
