46fe47d83269cdcab5e5c47261b2808d15a90bd6
Rmc99 <ronaldson_castro@hotmail.com>
2019-02-12 11:12:04 -0300

    Alterar nome padrão do django admin.

--- pagamento/models.py
+++ pagamento/models.py
@@ -41,9 +41,9 @@ class Pagamento(models.Model):
     valor_hora = models.DecimalField(max_digits=10, decimal_places=2, null=False, verbose_name="Valor da Hora")
     valor_pensao = models.DecimalField(max_digits=10, decimal_places=2, null=False, verbose_name="Pensão Alimentícia")
     valor_bruto = models.DecimalField(max_digits=10, decimal_places=2, null=False, verbose_name="Valor Bruto", default=0)
-    valor_inss = models.DecimalField(max_digits=10, decimal_places=2, null=False, verbose_name="Previdencia Oficial(INSS)", default=0, help_text="Taxa de 11%")
+    valor_inss = models.DecimalField(max_digits=10, decimal_places=2, null=False, verbose_name="Previdencia Oficial(INSS)", default=0, help_text="Desconto de 11%")
     valor_iss = models.DecimalField(max_digits=10, decimal_places=2, null=False,
-                                              verbose_name="Valor Desconto ISS", default=0, help_text="Taxa de 5%")
+                                              verbose_name="Valor Desconto ISS", default=0, help_text="Desconto de 5%")
     qtd_dependente_irpf = models.IntegerField(null=False, verbose_name="Dependentes(quantidade)", default=0, help_text="O valor da dedução é R$ 189,59 mensais, por dependente." )
     deducao_irpf = models.DecimalField(max_digits=10, decimal_places=2, null=False,
                                               verbose_name="Total de Deduções", default=0)
@@ -54,7 +54,7 @@ class Pagamento(models.Model):
     valor_liquido = models.DecimalField(max_digits=10, decimal_places=2, null=False,
                                               verbose_name="Valor Líquido", default=0)
     valor_patronal = models.DecimalField(max_digits=10, decimal_places=2, null=False,
-                                              verbose_name="Patronal", default=0, help_text="Taxa de 20%")
+                                              verbose_name="Patronal", default=0, help_text="Desconto de 20%")
     outras_deducoes = models.DecimalField(max_digits=10, decimal_places=2, null=False,
                                          verbose_name="Outras Deduções", default=0)
     dta_criacao = models.DateTimeField(editable=False, auto_now_add=True)
