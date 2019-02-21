b2f083f437979b2b64447a763dcaab10d9a0297e
ctic <ronaldson@ifma.edu.br>
2019-02-20 23:08:30 -0300

    Arrumado calculo com relação ao valor base, incluido pensão e outras deduções.

--- pagamento/urls.py
+++ pagamento/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import gerar_pdf
+from .views import gerar_pdf, relatorio_pagamento
 
 app_name = 'pagamento'
 urlpatterns = [
     path('pagamento/gerar_pdf', gerar_pdf, name='gerar_pdf'),
+    path('pagamento/relatorio_pagamento', relatorio_pagamento, name='relatorio_pagamento'),
 ]
--- pagamento/views.py
+++ pagamento/views.py
@@ -1,13 +1,22 @@
+from django.contrib.auth.decorators import login_required
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.template.loader import render_to_string
 from weasyprint import HTML
 from .models import *
 
-def gerar_pdf(request):
+@login_required
+def relatorio_pagamento(request):
+    l = Pagamento.objects.all()
+    return render(request, 'pagamento/relatorio_pagamento.html', {'lista': l})
+
+@login_required
+def gerar_pdf(request, ano):
     """Generate pdf."""
+    print(ano)
     # Model data
-    obj = Pagamento.objects.all().select_related('pessoa').all()
+    obj = Pagamento.objects.get(ano=ano).select_related('pessoa').all()
     # Rendered
     html_string = render_to_string('pagamento/pdf_template.html', {'lista': obj})
     html = HTML(string=html_string)
